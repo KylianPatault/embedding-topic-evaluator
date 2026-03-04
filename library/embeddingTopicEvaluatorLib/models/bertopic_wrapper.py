d47563f66cb8c6663249204a2ce9b65fc663b03b
Teissier Antoine <s2101052@gpue07.clusteretu.univ-lemans.fr>
2026-03-04 12:04:43 +0100

    modification doc

--- library/embeddingTopicEvaluatorLib/models/bertopic_wrapper.py
+++ library/embeddingTopicEvaluatorLib/models/bertopic_wrapper.py
@@ -10,7 +10,7 @@ from ..config.config import settings
 def load_model_BERTopic(config : dict = None) -> BERTopic:
     """
     Cette fonction permet de charger un modèle BERTopic à partir d'un fichier de configuration.
-    Si le fichier de configuration n'est pas fourni, la configuration par défaut est chargée (Situé dans le fichier config/config.py).
+    Si le fichier de configuration n'est pas fourni, la configuration par défaut est chargée (située dans le fichier config/config.py).
     La fonction retourne le modèle BERTopic charger. 
     """
     
@@ -20,13 +20,13 @@ def load_model_BERTopic(config : dict = None) -> BERTopic:
     umap_model = None 
     hdbscan_model = None 
 
-    # Vérification de la présence ou non d'un fichier de configuration pour UMAP, sinon défini à None. 
+    # Vérification de la présence ou non d'un fichier de configuration pour UMAP, si non défini à None. 
     if "UMAP" in config.keys() : 
         umap_config = config["UMAP"]
         umap_model = UMAP(n_neighbors=umap_config["n_neighbors"], n_components=umap_config["n_components"], 
                           min_dist=umap_config["min_dist"], metric=umap_config["metric"])
         
-    # Vérification de la présence ou non d'un fichier de configuration pour HDBSCAN, sinon défini à None. 
+    # Vérification de la présence ou non d'un fichier de configuration pour HDBSCAN, si non défini à None. 
     if "HDBSCAN" in config.keys() : 
         hdbscan_config = config["HDBSCAN"]
         hdbscan_model = HDBSCAN(min_cluster_size=hdbscan_config["min_cluster_size"], min_samples=hdbscan_config["min_samples"] , 
