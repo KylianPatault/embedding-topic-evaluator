85a0cb525a697e2d30a2e1ce28a51125fec719e9
Teissier Antoine <s2101052@gpue07.clusteretu.univ-lemans.fr>
2026-03-04 11:53:53 +0100

    modification doc + ajout configuration pour modèle KMEANS

--- library/embeddingTopicEvaluatorLib/config/config.py
+++ library/embeddingTopicEvaluatorLib/config/config.py
@@ -1,25 +1,43 @@
 class DefaultSettings():
     """Configuration par défaut des modèles"""
 
-    BERTOPIC_CONFIG = {
+    BERTOPIC_CONFIG_HDBSCAN = {
         "UMAP" : {
-            "n_neighbors" : 30,
-            "n_components" : 10,
-            "min_dist" : 0.0,
-            "metric" : "cosine"
+            "n_neighbors" : 15, # Taille du voisinage local (plus élevé = vision plus globale)
+            "n_components" : 5, # Dimension de sortie pour le clustering
+            "min_dist" : 0.0, # Distance min entre les points
+            "metric" : "cosine" # Métrique de distance
         },
         "HDBSCAN" : {
-            "min_cluster_size" : 50,
-            "min_samples" : 10,
-            "metric" :"euclidean",
-            "cluster_selection_method" : "eom",
-            "prediction_data" : True
+            "min_cluster_size" : 50, # Taille min d'un topic
+            "min_samples" : 10, # Sensibilité au bruit (plus bas = moins d'exclus)
+            "metric" :"euclidean", # Distance standard après UMAP
+            "cluster_selection_method" : "eom", # Sélection des clusters les plus denses
+            "prediction_data" : True # Permet de classer de nouveaux documents
         },
         "BERTopic" : {
+            # Modèle de Sentence Transformers utilisé pour les embeddings
             "embedding_model" : "all-mpnet-base-v2",
-            "nr_topics" : "auto",
-            "verbose" : True
+            "nr_topics" : "auto", # Réduction automatique du nombre de topics si nécessaire
+            "verbose" : True # Affiche la barre de progression
         }
     }
 
+    BERTOPIC_CONFIG_KMeans = {
+        "UMAP" : {
+            "n_neighbors" : 15, # Taille du voisinage local (plus élevé = vision plus globale)
+            "n_components" : 5, # Dimension de sortie pour le clustering
+            "min_dist" : 0.0, # Distance min entre les points
+            "metric" : "cosine" # Métrique de distance
+        },
+        "KMeans" : {
+            "n_clusters" : 50, # Nombre de thèmes (topics) ciblés
+        },
+        "BERTopic" : {
+            # Modèle de Sentence Transformers utilisé pour les embeddings
+            "embedding_model" : "all-mpnet-base-v2",
+            "nr_topics" : "auto", # Réduction automatique du nombre de topics si nécessaire
+            "verbose" : True # Affiche la barre de progression
+        }
+    }
 settings = DefaultSettings()
--- library/embeddingTopicEvaluatorLib/models/bertopic_wrapper.py
+++ library/embeddingTopicEvaluatorLib/models/bertopic_wrapper.py
@@ -6,36 +6,39 @@ from sklearn.cluster import KMeans
 
 from ..config.config import settings
 
-#Cette fonction permet de charger un modèle BERTopic à partir d'un fichier de configuration.
-#Si le fichier de configuration n'est pas fournis, c'est la configuration par défaut qui est chargé (Situé dans le fichier config/config.py).
-#La fonction retourne le modèle BERTopic charger. 
-def loadModelBERTopic(config : dict = None) -> BERTopic:
 
-    #Vérification de la présence ou non d'un fichier de configuration 
+def load_model_BERTopic(config : dict = None) -> BERTopic:
+    """
+    Cette fonction permet de charger un modèle BERTopic à partir d'un fichier de configuration.
+    Si le fichier de configuration n'est pas fourni, la configuration par défaut est chargée (Situé dans le fichier config/config.py).
+    La fonction retourne le modèle BERTopic charger. 
+    """
+    
+    # Vérification de la présence ou non d'un fichier de configuration 
     if config is None :
-        config = settings.BERTOPIC_CONFIG
+        config = settings.BERTOPIC_CONFIG_HDBSCAN
     umap_model = None 
     hdbscan_model = None 
 
-    #Vérification de la présence ou non d'un fichier de configuration pour UMAP, sinon fournis défini à None. 
+    # Vérification de la présence ou non d'un fichier de configuration pour UMAP, sinon fournis défini à None. 
     if "UMAP" in config.keys() : 
         umap_config = config["UMAP"]
         umap_model = UMAP(n_neighbors=umap_config["n_neighbors"], n_components=umap_config["n_components"], 
                           min_dist=umap_config["min_dist"], metric=umap_config["metric"])
         
-    #Vérification de la présence ou non d'un fichier de configuration pour HDBSCAN, sinon fournis défini à None. 
+    # Vérification de la présence ou non d'un fichier de configuration pour HDBSCAN, sinon fournis défini à None. 
     if "HDBSCAN" in config.keys() : 
         hdbscan_config = config["HDBSCAN"]
         hdbscan_model = HDBSCAN(min_cluster_size=hdbscan_config["min_cluster_size"], min_samples=hdbscan_config["min_samples"] , 
                                 metric=hdbscan_config["metric"], cluster_selection_method=hdbscan_config["cluster_selection_method"], 
                                 prediction_data=hdbscan_config["prediction_data"])
 
-    #Vérification de la présence ou non d'un fichier de configuration pour KMeans, si HDBSCAN non fournis, sinon fournis défini à None. 
+    # Vérification de la présence ou non d'un fichier de configuration pour KMeans, si HDBSCAN non fournis, sinon fournis défini à None. 
     elif "KMeans" in config.keys() :
         hdbscan_config = config["KMeans"]
         hdbscan_model = KMeans(n_clusters=hdbscan_config["n_clusters"]) 
 
-    #Création du modèle BERTopic  
+    # Création du modèle BERTopic  
     berTopic_config =  config["BERTopic"]
     topic_model = BERTopic(embedding_model=berTopic_config["embedding_model"], 
                            umap_model=umap_model, hdbscan_model=hdbscan_model, 
