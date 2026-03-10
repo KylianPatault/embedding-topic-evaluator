d06377ca98231d9de123674cc47e1602089375a5
Teissier Antoine <s2101052@gpue07.clusteretu.univ-lemans.fr>
2026-03-10 17:53:17 +0100

    Ajout de la nouvel métrique cohesion + modification diversity pour qu'elle soit toujours maximisée + modification centroide fonction, car il y avait une erreur.

--- library/embeddingTopicEvaluatorLib/metrics/diversity.py
+++ library/embeddingTopicEvaluatorLib/metrics/diversity.py
@@ -2,7 +2,6 @@
 
 from sklearn.metrics.pairwise import cosine_similarity
 import numpy as np
-from bertopic import BERTopic
 from typing import Callable
 
 from ..utils.embeddings import calculCentroide
@@ -11,8 +10,9 @@ from ..models.base import TopicModelEvaluator
 def diversity(model: TopicModelEvaluator, distance: Callable[[np.ndarray], np.ndarray] = cosine_similarity, 
               maximise: bool = True, useEmbeddingModel: bool = True )-> float :
     """
-    Cette fonction permet de calculer la diversité moyenne entre tous les centroides des topics en fonction de leurs mots de référence. 
-    La fonction requiert une instance de TopicModelEvaluator (encapsulant le modèle de topics), une métrique de comparaison des centroïdes (par défaut cosine_similarity), ainsi qu'un booléen définissant l'objectif d'optimisation (maximisation ou minimisation).
+    Cette fonction permet de calculer la diversité moyenne entre tous les centroïdes des topics en fonction de leurs mots de référence. 
+    Le but de la diversity est d'être maximisé. 
+    La fonction requiert une instance de TopicModelEvaluator (encapsulant le modèle de topics), une métrique de comparaison des centroïdes (par défaut cosine_similarity), ainsi qu'un booléen définissant l'objectif d'optimisation (maximisation ou minimisation) de la métrique .
     """
 
     keys = [k for k in model.getTopicsKeys() if k != -1]
@@ -31,8 +31,8 @@ def diversity(model: TopicModelEvaluator, distance: Callable[[np.ndarray], np.nd
     
     if maximise:
         np.fill_diagonal(matrix, -np.inf)
-        res = matrix.max(axis=1)
-    else:
+        res = 1 - matrix.max(axis=1)
+    else: 
         np.fill_diagonal(matrix, np.inf)
         res = matrix.min(axis=1)
         
